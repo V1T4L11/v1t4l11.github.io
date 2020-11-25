f7efb87d81c174c6efbd0c028d5ef5c0bac47eed
V1T4L11 <osvitalik@i.ua>
2020-11-25 16:56:21 +0200

    exchange function for if/else

--- articles/js-form/js-nightMode.js
+++ articles/js-form/js-nightMode.js
@@ -1,30 +1,26 @@
-// const select = document.querySelector('select');
-const html = document.querySelector('.main');
+const html = document.querySelector('.body');
 const getCheckbox = document.getElementById('checkNightMode');
 const setText = document.querySelector('.night-mode__text-set');
 
-getCheckbox.onclick = function() {
-  let choice = getCheckbox.checked;
+getCheckbox.addEventListener('change', e => {
+
+  let choice = e.target.checked;
   let bgColor;
   let textColor;
   let textContentMode;
 
-  switch (choice) {
-    case false:
-      bgColor = '#fff';
-      textColor = '#000000';
-      textContentMode = 'OFF';
-      break;
-    case true:
-      bgColor = '#000000';
-      textColor = '#fff';
-      textContentMode = 'ON';
-      break;
-    default:
-      break;
+  if (choice == true) {
+    bgColor = '#000000';
+    textColor = '#fff';
+    textContentMode = 'ON';
+  } else {
+    bgColor = '#fff';
+    textColor = '#000000';
+    textContentMode = 'OFF';
   }
+
   update(bgColor, textColor, textContentMode);
-}
+})
 
 function update(bgColor, textColor, textContentMode) {
     html.style.backgroundColor = bgColor;

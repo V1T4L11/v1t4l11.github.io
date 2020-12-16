43880b9856c3990b878292bf916ae5f176460a39
V1T4L11 <osvitalik@i.ua>
2020-12-16 16:12:25 +0200

    add test-reaction

---
+++ articles/test-reaction/js.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const div = document.querySelector('#div');
+
+const setTime = document.querySelector('.set-time');
+const figures = document.querySelectorAll('.figure');
+
+let start = new Date().getTime();
+
+function disappear() {
+  this.classList.remove('visible');
+  let end = new Date().getTime();
+  let timeTaken = (end - start) / 1000;
+  setTime.innerText = 'Time: ' + timeTaken + ' sec.';
+  start = new Date().getTime();
+  showFigure();
+};
+
+function showFigure() {
+  let randomEl = Math.floor(Math.random() * 3);
+  let randFigure = figures[randomEl];
+  let randomPositionX = Math.floor(Math.random() * 75);
+  let randomPositionY = Math.floor(Math.random() * 55);
+
+  randFigure.classList.add('visible');
+  randFigure.style.left = randomPositionX + '%';
+  randFigure.style.bottom = randomPositionY + '%';
+
+  randFigure.addEventListener('click', disappear);
+}
+
+document.addEventListener('DOMContentLoaded', showFigure);
